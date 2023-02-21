686358d41d9fe70b1e1da30ea9aaaf3d4f2f967f
jkcookiejk <cookjkcomputer@gmail.com>
2023-02-21 16:15:03 +0900

    Initial commit

---
+++ src/main/java/com/br/spring/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,46 @@
+package com.br.spring.member.model.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.spring.member.model.dao.MemberDao;
+import com.br.spring.member.model.vo.Member;
+
+@Service 
+public class MemberServiceImpl implements MemberService {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession; //선언만해서 null일것 같지만 나름대로 생성된 객체가 대입될 거임
+	
+	@Autowired 
+	private MemberDao mDao;
+	
+	@Override
+	public Member loginMember(Member m) {
+		Member loginUser = mDao.loginMember(sqlSession, m); 
+		return loginUser; 
+	}
+
+	@Override
+	public int insertMember(Member m) {
+		int result = mDao.insertMember(sqlSession, m); 
+		return result; 
+	}
+
+	@Override
+	public int updateMember(Member m) {
+		return 0;
+	}
+
+	@Override
+	public int deleteMember(String userId) {
+		return 0;
+	}
+
+	@Override
+	public int idCheck(String checkId) {
+		return 0;
+	}
+
+}
